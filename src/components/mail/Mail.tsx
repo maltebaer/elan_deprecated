e6d111af0cbf2bd7039968adfd5c03ccc98a54d1
Malte <m.felmy@googlemail.com>
2020-02-08 12:45:39 +0100

    added serve for production build.

--- src/components/mail/Mail.tsx
+++ src/components/mail/Mail.tsx
@@ -4,7 +4,7 @@ import {isMobile} from "react-device-detect";
 const FooterMail: React.FC = () => {
     return (
         <span className={`text-underline ${isMobile ? "small" : ""}`}>
-            <a href="#">info@elanberlin.com</a>
+            <a href="#">hello@elanberlin.com</a>
         </span>
     );
 };
